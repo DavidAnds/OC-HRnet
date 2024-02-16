de3b9931d113df83abad03593da65a5fdcd19fc5
Fitiavana David Andriason Rakotondratsimba <78416237+DavidAnds@users.noreply.github.com>
2024-02-16 12:06:56 +0100

    Employee init

--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -1,5 +1,14 @@
+import { TextInput } from "../components/Inputs"
+import { useForm } from "react-hook-form"
+
 function EmployeeList() {
-  return <div>List</div>
+  const { control, handleSubmit } = useForm()
+
+  return (
+    <div>
+      <TextInput control={control} name="name" label="Name" />
+    </div>
+  )
 }
 
 export default EmployeeList
