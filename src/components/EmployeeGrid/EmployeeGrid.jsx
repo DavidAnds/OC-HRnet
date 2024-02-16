6239f77b6c7b00a550e59bb75112f040b8fc6540
Fitiavana David Andriason Rakotondratsimba <78416237+DavidAnds@users.noreply.github.com>
2024-02-16 13:11:01 +0100

    EmployeeGrid implemented

---
+++ src/components/EmployeeGrid/EmployeeGrid.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { DataGrid } from "@mui/x-data-grid"
+
+function EmployeeGrid({ rows }) {
+  const columns = [
+    { field: "firstName", headerName: "First Name" },
+    { field: "lastName", headerName: "Last Name" },
+    { field: "startDate", headerName: "Start Date" },
+    { field: "department", headerName: "Department" },
+    { field: "dateOfBirth", headerName: "Date of Birth" },
+    { field: "street", headerName: "Street" },
+    { field: "city", headerName: "City" },
+    { field: "state", headerName: "State" },
+    { field: "zipCode", headerName: "Zip Code" },
+  ]
+
+  return (
+    <div>
+      <DataGrid rows={rows} columns={columns} />
+    </div>
+  )
+}
+
+export default EmployeeGrid
